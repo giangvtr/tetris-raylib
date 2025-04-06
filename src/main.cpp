eef27828aeac0d5f3e2793288872403346690484
Giang Vu <Tra-Giang.Vu@insa-rennes.fr>
2025-04-06 16:26:39 +0200

    Setup gameloop and window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
 #include <raylib.h>
 
 int main(){
+
+	//Set a blank window
 	InitWindow(300,600,"My Tetris");
+
+	//Set speed of our game
+	SetTargetFPS(60);
+
+
+	while(WindowShouldClose()==false){
+		BeginDrawing();
+		ClearBackground(DARKBLUE);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
 }
